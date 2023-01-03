37ded13a9bc689b4bf2eeab70cae1dec62d4aec6
Łukasz Stępień <lukaszatepien@gmail.com>
2023-01-03 19:50:21 +0100

    Create style after click and coordinates

--- 03.01.22/script.js
+++ 03.01.22/script.js
@@ -1,3 +1,4 @@
 document.querySelector('#something-3').addEventListener('click', () => {
     document.querySelector('link').setAttribute('href', 'after click.css');
+    document.querySelector('#something-2').textContent = `Współrzędne X: ${event.clientX} - Y: ${event.clientY}`;
 })
